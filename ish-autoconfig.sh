5e88e48ae40c3ac4b37ecb93b227a6ed63d12faf
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 16:41:54 -0500

    Updated to #!/bin/ash
    Add correct config for ash

--- ish-autoconfig.sh
+++ ish-autoconfig.sh
@@ -1,3 +1,4 @@
+#!/bin/ash
 echo http://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
 echo http://dl-cdn.alpinelinux.org/alpine/v3.10/main >> /etc/apk/repositories && \
 echo http://dl-cdn.alpinelinux.org/alpine/v3.10/community >> /etc/apk/repositories && \ 
