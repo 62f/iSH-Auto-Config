adef3168f883b403399819a9af68d18c8c278bea
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 22:43:17 -0500

    VNC Sever Config
    Need help with the viewing process

---
+++ VNC-Server-Setup.sh
@@ -0,0 +1,18 @@
+#!bin/ash
+# Starts VNC Server 
+#
+# Start backgrounding so we can connect with app not opencat 
+#
+    /dev/location > /dev/null & && \
+#
+# Make sure to always accept
+#
+# Start the VNC Server 
+#
+    x11vnc -create -noshm -forever
+#
+# Tip: add -forever to be able to quickly reconnect to a session if VNC disconnects
+#
+# Open VNC viewer and connect to 127.0.0.1:5900
+# 
+# open vnc viewer 127.0.0.1:5900
