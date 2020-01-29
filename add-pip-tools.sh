86e4794fffbb9d59d9601dfc5ff5fcfc88522fdb
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 22:17:34 -0500

    add-pip-tools.sh

---
+++ add-pip-tools.sh
@@ -0,0 +1,9 @@
+!#bin/ash
+# Installs additional tools using Pip
+pipx install eggshell && \
+pipx install scikit-fuzzy && \
+pipx install dnspython3 && \
+pip install brew && \
+pip install commix && \
+pip install ethtools && \
+sh other-tools.sh
