ebdc33b9f6845601f2e574f25c3b76522b2817ea
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 21:36:53 -0500

    Add-apks4

---
+++ add-apks5.sh
@@ -0,0 +1,33 @@
+#!/bin/ash
+apk add \
+    libtasn1 \
+    libtls-standalone \
+    libunistring \
+    libuuid \
+    libuv \
+    libxml2 \
+    libxml2-dev \
+    libxslt \
+    libxslt-dev \
+    linux-headers \
+    llvm \
+    lshw \
+    lua5.2-libs \
+    lua5.3-libs \
+    lz4-libs \
+    m4 \
+    macchanger \
+    make \
+    man \
+    mdocml \
+    mpc1 \
+    mpfr3 \
+    musl \
+    musl-dev \
+    musl-utils \
+    nano \
+    ncurses \
+    ncurses-dev \
+    ncurses-libs \
+    ncurses-terminfo --allow-untrusted && \
+sh add-apks5.sh
