2986729debfbf43cfcf418f4d46c35d5f87f3324
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 22:04:51 -0500

    Update add-apk-sources.sh

--- add-apk-sources.sh
+++ add-apk-sources.sh
@@ -1,8 +1,8 @@
 #!/bin/ash
 # Adds Source Repositories for APKs
-echo http://dl-cdn.alpinelinux.org/alpine/edge/community >> /etc/apk/repositories && \
-echo http://dl-cdn.alpinelinux.org/alpine/v3.10/main >> /etc/apk/repositories && \
-echo http://dl-cdn.alpinelinux.org/alpine/v3.10/community >> /etc/apk/repositories && \ 
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.10/main" >> /etc/apk/repositories && \
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories && \ 
 echo "http://nl.alpinelinux.org/alpine/v3.10/community" >> /etc/apk/repositories && \
 echo "http://nl.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
 apk update && \
