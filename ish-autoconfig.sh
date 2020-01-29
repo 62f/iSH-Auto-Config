97d29cedc6d2d960b749d384748b439ffb2baab0
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 21:49:44 -0500

    Update ish-autoconfig.sh

--- ish-autoconfig.sh
+++ ish-autoconfig.sh
@@ -1,9 +1,10 @@
 #!bin/ash
 chmod +x add-apk-sources.sh
 chmod +x add-apks.sh && \
-chmod +x add-apks.sh && \
-chmod +x add-apks.sh && \
-chmod +x add-apks.sh && \
-chmod +x add-apks.sh && \
-chmod +x add-apks.sh && \
-chmod +x pip-pipx-config.sh
+chmod +x add-apks2.sh && \
+chmod +x add-apks3.sh && \
+chmod +x add-apks4.sh && \
+chmod +x add-apks5.sh && \
+chmod +x add-apks6.sh && \
+chmod +x pip-pipx-config.sh && \
+sh add-apk-sources.sh
