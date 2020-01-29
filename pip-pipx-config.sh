91582cf44435b3b9876545e08876db268c4fb26a
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 21:41:24 -0500

    pip-pipx-config.sh

---
+++ pip-pipx-config.sh
@@ -0,0 +1,13 @@
+#!/bin/ash
+python3 -m pip install -U pip pipx --upgrade pip pipx setuptools wheel && \
+pipx ensurepath && \
+pipx completions && \
+git clone https://github.com/pypa/pipenv.git && \
+cd pipenv && \
+python3 setup.py install && \
+pipx install eggshell && \
+pipx install scikit-fuzzy && \
+pipx install dnspython3 && \
+pip install brew && \
+pip install commix && \
+pip install ethtools 
