1513871931834f7870f0c0e2c9ffbc8b6c1c6ffd
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-29 00:07:48 -0500

    Update VNC-Server-Setup.sh

--- VNC-Server-Setup.sh
+++ VNC-Server-Setup.sh
@@ -1,11 +1,13 @@
 #!bin/ash
 # Starts VNC Server 
-#
+
 # Start background daemon so we can connect with app not opencat 
+# Make sure to always accept  
     /dev/location > /dev/null & && \
-# Make sure to always accept
+
 # Start the VNC Server 
-    x11vnc -create -noshm -forever
 # Tip: add -forever to be able to quickly reconnect to a session if VNC disconnects
-# Open VNC viewer and connect to 127.0.0.1:5900
+    x11vnc -create -noshm -forever
+
+# Open VNC viewer & connect to 127.0.0.1:5900
 #   127.0.0.1:5900
