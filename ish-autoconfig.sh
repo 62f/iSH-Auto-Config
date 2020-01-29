4a71d708c3fe7fec556433c1f283fff71adbb958
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 21:48:05 -0500

    ish-autoconfig.sh
    Make files executable

---
+++ ish-autoconfig.sh
@@ -0,0 +1,9 @@
+#!bin/ash
+chmod +x add-apk-sources.sh
+chmod +x add-apks.sh && \
+chmod +x add-apks.sh && \
+chmod +x add-apks.sh && \
+chmod +x add-apks.sh && \
+chmod +x add-apks.sh && \
+chmod +x add-apks.sh && \
+chmod +x pip-pipx-config.sh
