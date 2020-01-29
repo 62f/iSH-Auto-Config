1c8e91d245b26a903a86af084ab60365486b603a
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-28 21:40:54 -0500

    Add-apks6

---
+++ add-apks6.sh
@@ -0,0 +1,36 @@
+#!/bin/ash
+apk add \
+    python3 \
+    python3-dev \
+    readline \
+    readline-dev \
+    ruby \
+    ruby-bigdecimal \
+    ruby-bundler \
+    ruby-dev \
+    ruby-etc \
+    ruby-io-console \
+    ruby-libs \
+    scanelf \
+    serf \
+    setuptools \
+    snort \
+    sqlite \
+    sqlite-dev \
+    sqlite-libs \
+    ssl_client \
+    subversion \
+    subversion-libs \
+    tshark \
+    tzdata \
+    wget \
+    wireless-tools \
+    wireshark-common \
+    xpat \
+    xz-libs \
+    yaml \
+    yaml-dev \
+    yersinia \
+    zlib \
+    zlib-dev --allow-untrusted && \
+sh pip-pipx-config.sh
