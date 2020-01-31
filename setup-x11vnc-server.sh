aa54c96736d3a9efa9de6f425b9c67746843b278
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-01-30 21:56:42 -0500

    Update setup-x11vnc-server.sh

--- setup-x11vnc-server.sh
+++ setup-x11vnc-server.sh
@@ -11,9 +11,22 @@ cat /dev/location > "/dev/null &" && \
 # Select "While Using App"
 # Select "Always"  
 
-# Starts VNC Server 
-x11vnc -create -noshm -forever
+# Starts x11 VNC Server 
+x11vnc -create -noshm -forever && \
+
+# Set A Password to Connect
+# x11vnc -rfbauth ~/.vnc/passwd
+# Use Your Own VNC Passwd File 
+# x11vnc -rfbauth /PATH/file
+# Disable Password Usage -nopw 
+# For More Info on Passwd
+# http://www.karlrunge.com/x11vnc/faq.html#faq-passwd
+
+# To Use x11VNC SSL Tunneling 
+# -ssl -stunnel
+# For More Info on SSH Tunneling 
+# http://www.karlrunge.com/x11vnc/#tunnelling   
+x11vnc -rfbauth -usepw -ssl -stunnel ~/.vnc/passwd && \
 
 # Open VNC Viewer
 # Connect to 127.0.0.1:5900
-
