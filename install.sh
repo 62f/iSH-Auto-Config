b971acbe4163450726e7bfd57d861a0e7536be68
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-02-03 21:19:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ chmod +x add-apks4.sh && \
 chmod +x add-apks5.sh && \
 chmod +x add-apks6.sh && \
 chmod +x pip-pipx-pipenv-config.sh && \
-chmod +x add-pips.sh
+chmod +x add-pips.sh && \
+chmod +x pentesting-tools.sh && \
 chmod +x other-tools.sh && \
-chmod +x setup-ssh.sh
-chmod +x add-py3-apks.sh \
+chmod +x setup-ssh.sh && \
+chmod +x add-py3-apks.sh && \
 chmod +x setup-x11vnc-server.sh && \
 sh add-apk-sources.sh
