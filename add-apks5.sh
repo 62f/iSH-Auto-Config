909789a60c33ed0e2e35709f8bd3b54dd568385a
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-02-03 21:06:48 -0600

    Update add-apks5.sh

--- add-apks5.sh
+++ add-apks5.sh
@@ -6,6 +6,40 @@ apk add \
     nettle \
     nghttp2-libs \
     nginx \
+    nginx-doc \
+    nginx-mod-devel-kit \
+    nginx-mod-http-cache-purge \
+    nginx-mod-http-echo \
+    nginx-mod-http-fancyindex \
+    nginx-mod-http-geoip \
+    nginx-mod-http-geoip2 \
+    nginx-mod-http-headers-more \
+    nginx-mod-http-image-filter \
+    nginx-mod-http-js \
+    nginx-mod-http-lua \
+    nginx-mod-http-lua-upstream \
+    nginx-mod-http-nchan \
+    nginx-mod-http-perl \
+    nginx-mod-http-redis2 \
+    nginx-mod-http-set-misc \
+    nginx-mod-http-shibboleth \
+    nginx-mod-http-upload-progress \
+    nginx-mod-http-upstream-fair
+    nginx-mod-http-vod \
+    nginx-mod-http-xslt-filter \
+    nginx-mod-mail \
+    nginx-mod-rtmp \
+    nginx-mod-stream-geoip \
+    nginx-mod-stream-geoip2 \
+    nginx-mod-stream-js \
+    nginx-vim \
+    bareos-webui-nginx \
+    nginx-mod-http-headers-more
+    nginx-mod-http-image-filter \
+    nginx-mod-http-upstream-fair \
+    nginx-mod-http-vod \
+    nginx-mod-stream \
+    patchwork-uwsgi-nginx \
     nikto \
     nmap \
     nnn \
