f8ff7de1058318f1546089bd58857d43452bb8ef
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-02-19 00:28:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!bin/ash
 # Makes .sh scripts executable
-chmod +x add-apk-sources.sh && \
-chmod +x add-apks.sh && \
-chmod +x add-apks2.sh && \
-chmod +x add-apks3.sh && \
-chmod +x add-apks4.sh && \
-chmod +x add-apks5.sh && \
-chmod +x add-apks6.sh && \
-chmod +x pip-pipx-pipenv-config.sh && \
-chmod +x add-pips.sh && \
-chmod +x pentesting-tools.sh && \
-chmod +x other-tools.sh && \
-chmod +x setup-ssh.sh && \
-chmod +x add-py3-apks.sh && \
-chmod +x setup-x11vnc-server.sh && \
+chmod 777 add-apk-sources.sh && \
+    add-apks.sh \
+    add-apks2.sh \
+    add-apks3.sh \
+    add-apks4.sh \
+    add-apks5.sh \
+    add-apks6.sh \
+    pip-pipx-pipenv-config.sh \
+    add-pips.sh \
+    pentesting-tools.sh \
+    other-tools.sh \
+    setup-ssh.sh \
+    add-py3-apks.sh \
+    setup-x11vnc-server.sh \
+    add-apks.sh && \
 sh add-apks.sh
