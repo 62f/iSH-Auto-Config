6a4c1d73a5375934f70f7d311c8ed3a3dd47a2b8
Z3R0_Days:~# <56379955+ProjectZeroDays@users.noreply.github.com>
2020-07-15 20:36:58 -0500

    Update pip-pipx-pipenv-config.sh

--- pip-pipx-pipenv-config.sh
+++ pip-pipx-pipenv-config.sh
@@ -1,4 +1,8 @@
 #!/bin/ash
+
+#Installs Requirements to use as Standalone
+apk add python3 && \
+
 # Installs Pip, Pipx, and Setup For PipEnv 
 python3 -m pip install -U pip pipx --upgrade pip pipx setuptools wheel && \
 pipx ensurepath && \
@@ -7,5 +11,5 @@ git clone https://github.com/pypa/pipenv.git && \
 cd pipenv && \
 python3 setup.py install && \
 clear && \
-fish && \
+ash && \
 sh add-pips.sh 
